a8833b28ef1402a1819b16dece3ff6fc193eb993
Hobbs, Lorenzo <lorenzo.j.hobbs@gmail.com>
2026-09-13 01:09:59 -0400

    Added reed switch event handling

---
+++ button_test.py
@@ -0,0 +1,11 @@
+from gpiozero import Button
+
+button = Button(17)
+
+while True: 
+
+	if button.is_pressed:
+		print("Button is pressed")
+
+	else: 
+		print("Button is not pressed")
--- security_system.py
+++ security_system.py
@@ -1,32 +1,51 @@
 from gpiozero import Button
 
 currentState = "DISARMED"
-button = Button(17)
 
+
+reed_switch = Button(17)
+
+#function to tell the door has been opened
+def doorOpened():
+	global currentState
+
+	
+     #if we are in ARMED mode 
+	if currentState == "ARMED":
+		print("Door Opened")
+		currentState = "ALARMING"
+		print("ATTN ALARMING")
+
+reed_switch.when_released = doorOpened
+
+
+#security system is active
 securitySystem = "RUNNING"
-
+#---------------------BEGIN LOOP-------------------------
 while securitySystem == "RUNNING":
-
-
+	
 	if currentState == "DISARMED": 
 		print("DISARMED")
-
+		#ask for arm code and store in variable
 		arm_code = input("Please enter the arm code:")
-
+		#if the code is correct 
 		if arm_code == "999":
+		#new state is armed
 			currentState = "ARMED"
+			print("ARMED")
+		#if code is not correct prompt user for code again
 		else: 
-			print("Invalid arm code.") 
-	if currentState == "ARMED":
-		print("ARMED")
-		if button.is_pressed: 
-			currentState = "ALARMING"
-			print("ATTN ALARMING!!")
+			print("Invalid arm code.") 	
+	# if we are in alarm mode
 	if currentState == "ALARMING":
-		disarm_code =input("Please enter the arm code: ")
-		if disarm_code == "999":
-			print("DISARMED")
-			currentState = "DISARMED"
+			# prompt user to put in the disarm code
+			disarm_code =input("Please enter the disarm code: ")
+			# if the disarm code is correct
+			if disarm_code == "999":
+				# show we are disarmed
+				print("DISARMED")
+				currentState = "DISARMED"
+	#---------------------------------END LOOP------------------------------
 
   
 
